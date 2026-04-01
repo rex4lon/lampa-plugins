5192b32e74f2467eca9168305afcf2a3d8d4cda4
rex4lon <syl5ron@gmail.com>
2026-04-01 12:09:10 +0300

    Update test-ts.js

--- test-ts.js
+++ test-ts.js
@@ -8,7 +8,7 @@
     }  
   
     // Настройки стороннего TorrServer  
-    Lampa.Storage.set('torrserver_url', 'http://176.117.76.60:8090/');  
+    Lampa.Storage.set('torrserver_url', 'http://176.117.76.60:8090');  
     Lampa.Storage.set('torrserver_auth', 'true');  
     Lampa.Storage.set('torrserver_login', 'novanetua');  
     Lampa.Storage.set('torrserver_password', 'framo');  
